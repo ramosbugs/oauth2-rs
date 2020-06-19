a93cbdf16c5636487aa0149475194a31542c1aee
Marc Mettke <marc@itmettke.de>
2020-06-19 11:34:43 +0200

    Removing future 0.1 and reqwest 0.9
    * future 0.1 and reqwest 0.9 are both pretty old
      and should not be used anymore. Thus we switch to
      only supporting future 0.3 and reqwest 0.10
    * unicode-normalization is unused and thus removed
    * tokio was used inside future 0.1 and now get removed

--- src/reqwest/mod.rs
+++ src/reqwest/mod.rs
@@ -22,41 +22,28 @@ where
     Other(String),
 }
 
-#[cfg(any(feature = "reqwest-09", feature = "reqwest-010"))]
+#[cfg(feature = "reqwest-010")]
 pub use blocking::http_client;
 ///
 /// Error type returned by failed reqwest blocking HTTP requests.
+/// Requires "reqwest-010" feature.
 ///
-#[cfg(any(feature = "reqwest-09", feature = "reqwest-010"))]
+#[cfg(feature = "reqwest-010")]
 pub type HttpClientError = Error<blocking::reqwest::Error>;
-
-#[cfg(all(feature = "futures-01", feature = "reqwest-09"))]
-pub use future_client::future_http_client;
-///
-/// Error type returned by failed reqwest futures HTTP requests.
-///
-#[cfg(all(feature = "futures-01", feature = "reqwest-09"))]
-pub type FutureHttpClientError = Error<reqwest_0_9::Error>;
 
 #[cfg(all(feature = "futures-03", feature = "reqwest-010"))]
 pub use async_client::async_http_client;
 ///
 /// Error type returned by failed reqwest async HTTP requests.
+/// Requires "futures-03" and "reqwest-010" feature.
 ///
 #[cfg(all(feature = "futures-03", feature = "reqwest-010"))]
 pub type AsyncHttpClientError = Error<reqwest_0_10::Error>;
 
-#[cfg(any(feature = "reqwest-09", feature = "reqwest-010"))]
+#[cfg(feature = "reqwest-010")]
 mod blocking {
     use super::super::{HttpRequest, HttpResponse};
     use super::Error;
-
-    #[cfg(all(feature = "reqwest-09", not(feature = "reqwest-010")))]
-    use reqwest_0_9 as blocking;
-    #[cfg(all(feature = "reqwest-09", not(feature = "reqwest-010")))]
-    pub use reqwest_0_9 as reqwest;
-    #[cfg(all(feature = "reqwest-09", not(feature = "reqwest-010")))]
-    use reqwest_0_9::RedirectPolicy;
 
     #[cfg(feature = "reqwest-010")]
     pub use reqwest_0_10 as reqwest;
@@ -77,10 +64,6 @@ mod blocking {
             .build()
             .map_err(Error::Reqwest)?;
 
-        #[cfg(all(feature = "reqwest-09", not(feature = "reqwest-010")))]
-        let mut request_builder = client
-            .request(request.method, request.url.as_str())
-            .body(request.body);
         #[cfg(feature = "reqwest-010")]
         let mut request_builder = client
             .request(
@@ -100,14 +83,6 @@ mod blocking {
         let mut body = Vec::new();
         response.read_to_end(&mut body).map_err(Error::Io)?;
 
-        #[cfg(all(feature = "reqwest-09", not(feature = "reqwest-010")))]
-        {
-            Ok(HttpResponse {
-                status_code: response.status(),
-                headers: response.headers().clone(),
-                body,
-            })
-        }
         #[cfg(feature = "reqwest-010")]
         {
             let headers = response
@@ -129,60 +104,6 @@ mod blocking {
                 body,
             })
         }
-    }
-}
-
-#[cfg(all(feature = "futures-01", feature = "reqwest-09"))]
-mod future_client {
-    use super::super::{HttpRequest, HttpResponse};
-    use super::Error;
-
-    use futures_0_1::{Future, IntoFuture, Stream};
-    use reqwest_0_9 as reqwest;
-    use reqwest_0_9::r#async::Client as AsyncClient;
-
-    ///
-    /// Asynchronous HTTP client.
-    ///
-    pub fn future_http_client(
-        request: HttpRequest,
-    ) -> impl Future<Item = HttpResponse, Error = Error<reqwest::Error>> {
-        AsyncClient::builder()
-            // Following redirects opens the client up to SSRF vulnerabilities.
-            .redirect(reqwest::RedirectPolicy::none())
-            .build()
-            .map_err(Error::Reqwest)
-            .into_future()
-            .and_then(|client| {
-                let mut request_builder = client
-                    .request(request.method, request.url.as_str())
-                    .body(request.body);
-                for (name, value) in &request.headers {
-                    request_builder = request_builder.header(name, value);
-                }
-                request_builder
-                    .build()
-                    .map_err(Error::Reqwest)
-                    .into_future()
-                    .and_then(move |request| {
-                        client
-                            .execute(request)
-                            .and_then(|response| {
-                                let status_code = response.status();
-                                let headers = response.headers().clone();
-                                response
-                                    .into_body()
-                                    .map(|chunk| chunk.as_ref().to_vec())
-                                    .collect()
-                                    .map(move |body| HttpResponse {
-                                        status_code,
-                                        headers,
-                                        body: body.into_iter().flatten().collect::<_>(),
-                                    })
-                            })
-                            .map_err(Error::Reqwest)
-                    })
-            })
     }
 }
 

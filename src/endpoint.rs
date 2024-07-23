9fadfd59c03e635990b0a757c33866d5cb15253d
Benjamin Bouvier <public@benj.me>
2024-07-23 12:39:51 +0200

    Make `AsyncHttpClient::Future` `Send` on non-wasm archs

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -25,7 +25,12 @@ pub trait AsyncHttpClient<'c> {
     type Error: Error + 'static;
 
     /// Future type returned by HTTP client.
+    #[cfg(target_arch = "wasm32")]
     type Future: Future<Output = Result<HttpResponse, Self::Error>> + 'c;
+
+    /// Future type returned by HTTP client.
+    #[cfg(not(target_arch = "wasm32"))]
+    type Future: Future<Output = Result<HttpResponse, Self::Error>> + Send + 'c;
 
     /// Perform a single HTTP request.
     fn call(&'c self, request: HttpRequest) -> Self::Future;
@@ -33,7 +38,7 @@ pub trait AsyncHttpClient<'c> {
 impl<'c, E, F, T> AsyncHttpClient<'c> for T
 where
     E: Error + 'static,
-    F: Future<Output = Result<HttpResponse, E>> + 'c,
+    F: Future<Output = Result<HttpResponse, E>> + Send + 'c,
     // We can't implement this for FnOnce because the device authorization flow requires clients to
     // supportmultiple calls.
     T: Fn(HttpRequest) -> F,
